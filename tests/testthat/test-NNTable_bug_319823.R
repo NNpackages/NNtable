b70b248d6861b4d24cd812af8e0d78032e9b7064
SFFL (Steffen Falgreen Larsen) <sffl@novonordisk.com>
2021-01-25 13:23:38 +0100

    added tests for #319823

---
+++ tests/testthat/test-NNTable_bug_319823.R
@@ -0,0 +1,109 @@
+
+# test created for bug 319823
+
+# If the two inputs `format_alone = ` and `format_data =` are used, when applying
+# the addExposure function then it seems to interfere with the way
+# addFormat formats numbers.
+
+context("Test NNTable bug 319823")
+
+
+nntable_dir <- gsub("/NNtable-tests/testthat|/tests/testthat", "", getwd())
+output_path <- file.path(nntable_dir, "tests",  "output")
+
+dir.create(file.path(output_path, "got"), showWarnings = FALSE)
+
+
+(db <- nnaccess("0001", root = "~/training"))
+
+adae_f <- db$adam("adae") %>% filter(SAFFL == "Y" & TRTA != "")
+adsl_f <- db$adam("adsl") %>% filter(SAFFL == "Y" & TRT30A != "") %>%
+  rename(TRTA = TRT30A)
+
+
+subjects <- adsl_f %>%
+  group_by(TRTA) %>%
+  summarise("Number of subjects" = n_distinct(USUBJID),
+            "Total exposure" = sum(TRDURY),
+            .groups = "drop")
+
+
+stats <- adae_f %>%
+  group_by(TRTA,AESOC) %>%
+  summarise(N = n_distinct(USUBJID),
+            E = n(), .groups = "drop")
+
+
+table <- NNTable(stats, "TRTA","AESOC","N","E") %>%
+  addTransWide("TRTA" = c("N","E")) %>%
+  addGroupedColumns("AESOC") %>%
+  addOrder(E = -1)
+
+
+
+test_that("Bug 319823 baseline", {
+
+  .NNTable <- table
+
+  file <- "table_319823"
+
+  # print(.NNTable, file = file.path(output_path, "expected", file))
+
+  print(.NNTable, file = file.path(output_path, "got", file))
+
+  expect_equal(readLines(file.path(output_path, "expected", file)),
+               readLines(file.path(output_path, "got",      file)))
+})
+
+
+test_that("Bug 319823 baseline + exp", {
+
+  .NNTable <- table %>%
+    addExposure(exposure = subjects,
+                format_alone = TRUE,
+                format_data = c("Number of subjects" = "%.0f", "Total exposure" = "%.2f"))
+
+
+  file <- "table_1_319823"
+
+  # print(.NNTable, file = file.path(output_path, "expected", file))
+
+  print(.NNTable, file = file.path(output_path, "got", file))
+
+  expect_equal(readLines(file.path(output_path, "expected", file)),
+               readLines(file.path(output_path, "got",      file)))
+})
+
+test_that("Bug 319823 baseline + filling ", {
+
+  .NNTable <- table %>% addFilling(N = 0)
+
+  file <- "table_2_319823"
+
+  # print(.NNTable, file = file.path(output_path, "expected", file))
+
+  print(.NNTable, file = file.path(output_path, "got", file))
+
+  expect_equal(readLines(file.path(output_path, "expected", file)),
+               readLines(file.path(output_path, "got",      file)))
+})
+
+test_that("Bug 319823 baseline + filling + exp", {
+
+  .NNTable <- table %>%
+    addExposure(exposure = subjects,
+                format_alone = TRUE,
+                format_data = c("Number of subjects" = "%.0f", "Total exposure" = "%.2f")) %>%
+    addFilling(N = 0)
+
+  file <- "table_3_319823"
+
+  # print(.NNTable, file = file.path(output_path, "expected", file))
+
+  print(.NNTable, file = file.path(output_path, "got", file))
+
+  expect_equal(readLines(file.path(output_path, "expected", file)),
+               readLines(file.path(output_path, "got",      file)))
+})
+
+
